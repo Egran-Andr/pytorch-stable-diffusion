cc8c07260fc66f8bbcb22aef446a0adc572ae9a7
Egor Andreev <mlgrainbowfrog2003@gmail.com>
2024-02-02 19:04:53 +0300

    Created Text-to image

--- sd/pipeline.py
+++ sd/pipeline.py
@@ -1,8 +1,7 @@
 import torch
 import numpy as np
 from tqdm import tqdm
-
-
+from ddpm import DDPMSampler
 
 WIDTH = 512
 HEIGHT = 512
@@ -11,17 +10,95 @@ LATENTS_HEIGHT = HEIGHT // 8
 
 
 def generate(
-    prompt : str,
-    uncond_prompt : str,    #Negative prompt
-    input_image=None,       #image to image-to image generation
-    strength=0.8,           #attention to starting image
-    do_cfg=True,
-    cfg_scale=7.5,          #weight of prompt(number from 1 to 14)
-    sampler_name="ddpm",
-    n_inference_steps=50,
-    models={},              #pretrained model link
-    seed=None,
-    device=None,
-    idle_device=None,
-    tokenizer=None,
+        prompt: str,
+        uncond_prompt: str,  # Negative prompt or empty string
+        input_image=None,  # image to image-to image generation
+        strength=0.8,  # attention to starting image
+        do_cfg=True,
+        cfg_scale=7.5,  # weight of prompt(number from 1 to 14)
+        sampler_name="ddpm",
+        n_inference_steps=50,  # noice encode and decode steps
+        models={},  # pretrained model link
+        seed=None,
+        device=None,
+        idle_device=None,
+        tokenizer=None,
 ):
+    with torch.no_grad():
+        if not 0 < strength <= 1:
+            raise ValueError("Strength must be between 0 and 1")
+
+        if idle_device:
+            to_idle = lambda x: x.to(idle_device)
+        else:
+            to_idle = lambda x: x
+
+        # Initialize random number generator according to the seed specified
+        generator = torch.Generator(device=device)
+        if seed is None:
+            generator.seed()
+        else:
+            generator.manual_seed(seed)
+
+        clip = models["clip"]
+        clip.to(device)
+
+        if do_cfg:
+            # Convert into a list of length Seq_Len=77
+            cond_tokens = tokenizer.batch_encode_plus(
+                [prompt], padding="max_length", max_length=77
+            ).input_ids
+            # (Batch_Size, Seq_Len)
+            cond_tokens = torch.tensor(cond_tokens, dtype=torch.long, device=device)
+            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            cond_context = clip(cond_tokens)
+            # Convert into a list of length Seq_Len=77
+            uncond_tokens = tokenizer.batch_encode_plus(
+                [uncond_prompt], padding="max_length", max_length=77
+            ).input_ids
+            # (Batch_Size, Seq_Len)
+            uncond_tokens = torch.tensor(uncond_tokens, dtype=torch.long, device=device)
+            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            uncond_context = clip(uncond_tokens)
+            # (Batch_Size, Seq_Len, Dim) + (Batch_Size, Seq_Len, Dim) -> (2 * Batch_Size, Seq_Len, Dim)
+            context = torch.cat([cond_context, uncond_context])
+        else:
+            # Convert into a list of length Seq_Len=77
+            tokens = tokenizer.batch_encode_plus(
+                [prompt], padding="max_length", max_length=77
+            ).input_ids
+            # (Batch_Size, Seq_Len)
+            tokens = torch.tensor(tokens, dtype=torch.long, device=device)
+            # (Batch_Size, Seq_Len) -> (Batch_Size, Seq_Len, Dim)
+            context = clip(tokens)
+        to_idle(clip)
+
+        if sampler_name == "ddpm":
+            sampler = DDPMSampler(generator)
+            sampler.set_inference_timesteps(n_inference_steps)
+        else:
+            raise ValueError("Unknown sampler value %s. ")
+
+        latents_shape = (1, 4, LATENTS_HEIGHT, LATENTS_WIDTH)
+
+
+
+
+def rescale(x, old_range, new_range, clamp=False):
+    old_min, old_max = old_range
+    new_min, new_max = new_range
+    x -= old_min
+    x *= (new_max - new_min) / (old_max - old_min)
+    x += new_min
+    if clamp:
+        x = x.clamp(new_min, new_max)
+    return x
+
+
+def get_time_embedding(timestep):
+    # Shape: (160,)
+    freqs = torch.pow(10000, -torch.arange(start=0, end=160, dtype=torch.float32) / 160) #positional encoding
+    # Shape: (1, 160)
+    x = torch.tensor([timestep], dtype=torch.float32)[:, None] * freqs[None]
+    # Shape: (1, 160 * 2)
+    return torch.cat([torch.cos(x), torch.sin(x)], dim=-1)
